54fc2045ad25620677942bbbf2b0ea855bf51d89
hyorard-b <reddream08@gmail.com>
2021-01-17 23:43:58 +0900

    Update Dev Env
    add webpack entry, output dir
    add sass-loader, css-loader, MiniCssExtractPlugin
    add CleanWebpackPlugin

---
+++ client/webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  // entry file
+  // https://webpack.js.org/configuration/entry-context/#entry
+  entry: ['@babel/polyfill', './src/js/main.js'],
+  // 번들링된 js 파일의 이름(filename)과 저장될 경로(path)를 지정
+  // https://webpack.js.org/configuration/output/#outputpath
+  // https://webpack.js.org/configuration/output/#outputfilename
+  output: {
+    path: path.resolve(__dirname, 'dist/js'),
+    filename: 'bundle.js'
+  },
+  // https://webpack.js.org/configuration/module
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: [path.resolve(__dirname, 'src/js')],
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        ],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        // sass-loader : compiles Sass to CSS, using Node Sass by default
+        // css-loader : translates CSS into CommonJS
+        // use는 뒤에서부터 실행
+        exclude: /node_modules/
+      }
+    ]
+  },
+  devtool: 'source-map',
+  // https://webpack.js.org/configuration/mode
+  mode: 'development',
+  plugins: [
+    new CleanWebpackPlugin('bundle.js'), //
+    new MiniCssExtractPlugin({ filename: 'dist/css/style.css' }) //  bundle.js 파일에 컴파일된 css를 포함시키지 말고 별도의 css 파일로 분리해서 하나의 파일로 번들링
+  ]
+};
