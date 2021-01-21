c69bb2c82cc4afdd4da012618973dc981d291709
yg-0103 <rladusrn89@gmail.com>
2021-01-21 17:02:51 +0900

    feat: add description

--- client/src/js/render.js
+++ client/src/js/render.js
@@ -3,6 +3,7 @@ import timeState from './timeState';
 import fetchData from './axios/fetch-data';
 
 export default async function () {
+  // 서버에서 설정된 시간들을 가져온다.
   try {
     const {
       pomo_time,
@@ -10,7 +11,7 @@ export default async function () {
       long_break,
       long_interval,
     } = await fetchData();
-
+    // 상태에 따라 어떤시간을 렌더링할지 정한다.
     const getNowState = () => {
       return timeState.state === 'pomodoro'
         ? +pomo_time
@@ -18,14 +19,16 @@ export default async function () {
         ? +short_break
         : +long_break;
     };
-
+    // 설정된 시간이 0분이면 1분을 넣어준다.
     const curTime = getNowState() || 1;
 
     const pomodoro = new Pomodoro(curTime, 0, long_interval);
+    // 설정된 시간을 랜더링한다.
     pomodoro.setTimeText();
 
     const $nav = document.querySelector('.main__btn-group');
     $nav.addEventListener('click', (e) => {
+      // 네비게이션 버튼이 클릭되면 현재 설정된 시간으로 초기화 되고 초기화 된 시간을 다시 랜더링한다.
       pomodoro.minute = getNowState();
       pomodoro.setTimeText();
     });
--- client/src/js/time.js
+++ client/src/js/time.js
@@ -21,14 +21,15 @@ export default class Pomodoro {
     this.$longBtn = document.querySelector('#long-break');
     this.$shortBtn = document.querySelector('#short-break');
     this.$pomoBtn = document.querySelector('#pomodoro');
-
+    // 시작 버튼이 클릭되면 버튼의 클래스를 토글해주고 타이머를 시작하고 버튼의 텍스트를 바꿔주고 딸깍 오디오를 재생시킨다.
     this.$startBtn.onclick = () => {
       this.$startBtn.classList.toggle('active');
       this.countDown();
       this.setBtnText();
       this.clickAudio.play();
     };
-
+    // 네비게이션 버튼이 클릭되면 버튼의 클래스 엑티브를 제거해주고 버튼의 텍스트를 다시 셋팅해주고
+    // 현재의 상태를 변경해주고 배경색과 버튼색을 변경해주고 타이머를 멈춘다.
     this.$nav.onclick = (e) => {
       if (e.target === e.currentTarget) return;
       this.$startBtn.classList.remove('active');
@@ -37,12 +38,12 @@ export default class Pomodoro {
       this.setColor();
       this.stopTimer();
     };
-
+    // 커스텀 클릭이벤트 발생시킬 용도
     this.$longBtn.onclick = () => {};
     this.$shortBtn.onclick = () => {};
     this.$pomoBtn.onclick = () => {};
   }
-
+  // 타이머를 시작시킨다.
   countDown() {
     if (this.timerId) {
       clearInterval(this.timerId);
@@ -51,6 +52,8 @@ export default class Pomodoro {
     }
 
     this.timerId = setInterval(() => {
+      // 0분 0초가 되면 커스텀 이벤트를 발생시키고,
+      // pomodoro, short-break, long-break 중 어느 상태로 변경해야 하는지 설정하고 타이머를 멈춘다
       if (!this.minute && !this.second) {
         ++this.intervalCount;
         this.$time.dispatchEvent(this.timeEnd);
@@ -64,7 +67,7 @@ export default class Pomodoro {
       this.setTimeText();
     }, 1000);
   }
-
+  // 상태나 설정된 long-beak-interval에 따라서 어떤 커스텀 이벤트를 발생시킬지 선택한다.
   selectTime() {
     if (timeState.state !== 'pomodoro') {
       return this.$pomoBtn.dispatchEvent(this.customEvent);
@@ -75,13 +78,13 @@ export default class Pomodoro {
       this.$shortBtn.dispatchEvent(this.customEvent);
     }
   }
-
+  // 스타트 버튼의 텍스트를 변경
   setBtnText() {
     this.$startBtn.textContent = this.$startBtn.matches('.active')
       ? 'STOP'
       : 'START';
   }
-
+  // 현재 상태를 변경한다.
   setState(target) {
     [...this.$nav.children].forEach((child) => {
       child.classList.toggle('active', target === child);
@@ -89,7 +92,7 @@ export default class Pomodoro {
 
     timeState.state = target.id;
   }
-
+  // 상태에 따라 스타트버튼과 배경색을 변경한다.
   setColor() {
     document.body.style.backgroundColor =
       timeState.state === 'pomodoro'
@@ -105,13 +108,13 @@ export default class Pomodoro {
         ? 'rgb(70, 142, 145)'
         : 'rgb(67, 126, 168)';
   }
-
+  // 현재 타이머를 랜더링한다.
   setTimeText() {
     this.$time.textContent = `${
       this.minute < 10 ? '0' + this.minute : this.minute
     }:${this.second < 10 ? '0' + this.second : this.second}`;
   }
-
+  // 타이머를 셋팅한다.
   setCount() {
     if (!this.second) {
       --this.minute;
@@ -120,7 +123,7 @@ export default class Pomodoro {
       --this.second;
     }
   }
-
+  // 타이머를 멈춘다
   stopTimer() {
     clearInterval(this.timerId);
     this.timerId = null;
