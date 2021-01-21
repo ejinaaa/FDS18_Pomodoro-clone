4d3476b4ad8bafcd0a53fb4a47c9ff8e16b9965e
hyorard-b <reddream08@gmail.com>
2021-01-21 21:48:53 +0900

    feat: Progress Bar

---
+++ client/src/js/progress-bar.js
@@ -0,0 +1,40 @@
+import { fetchCurTime } from './axios/fetch';
+
+const $startBtn = document.querySelector('.btn-start');
+const $progressBar = document.querySelector('.loading-bar');
+const $clock = document.querySelector('.main__time-set');
+const $nav = document.querySelector('.main__btn-group');
+
+const progress = 10;
+let progressTimerId = null;
+
+const setProgressBar = () => {
+  clearInterval(progressTimerId);
+  progressTimerId = null;
+  $progressBar.style.width = '620px';
+};
+
+export default function () {
+  $startBtn.addEventListener('click', async () => {
+    if (progressTimerId) {
+      clearInterval(progressTimerId);
+      progressTimerId = null;
+      return;
+    }
+
+    const curTime = await fetchCurTime();
+
+    progressTimerId = setInterval(() => {
+      const remainProgress = $progressBar.style.width.substring(
+        0,
+        $progressBar.style.width.length - 2
+      );
+      $progressBar.style.width =
+        remainProgress === 30 ? '0px' : `${remainProgress - progress}px`;
+    }, 1000 * curTime);
+  });
+
+  $clock.addEventListener('timeEnd', setProgressBar);
+
+  $nav.addEventListener('click', setProgressBar);
+}
